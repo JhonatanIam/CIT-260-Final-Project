dbad462d0d66fe9de023a66262c03ae818c6b78e
OneDeMillion <56438355+OneDeMillion@users.noreply.github.com>
2020-07-21 13:01:11 -0500

    Update AccountProfitSharing.java

--- Final/src/AccountProfitSharing.java
+++ Final/src/AccountProfitSharing.java
@@ -1,17 +1,32 @@
 
 public class AccountProfitSharing extends Account  {
-
+	
 	
 	AccountProfitSharing(String type, String hireDate, double balance) {
+		/**
+		* inherits parameters from superclass Account
+		* @param type
+		* @param hireDate
+		* @param balance
+		*/
+		
 		super(type, hireDate, balance);
 	}
 	
 	@Override
 	protected void display() { 
+		/**
+		* overrides primary display class to display profit sharing type, balance and vested balance
+		*/
+		
 		System.out.println(" - Account Type: " + getType() + "\n      Balance: " + getBalance() + "\n      Vested Balance: " + getVested());
 	}
 
 	protected String saving() {
+		/**
+		* returns profit sharing type, hire date and balance
+		*/
+		
 		return getType() + " " + getHireDate() + " " + getBalance();
 	}
 	
